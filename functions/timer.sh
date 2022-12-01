26985d54d764753b00c34168f0c11d6b210f007b
perllb <perllbrattaas@gmail.com>
2022-12-01 10:32:37 +0100

    first commit; timer

---
+++ functions/timer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# function to time command passed as $1
+# input
+# $1: command to execute as string
+timeCMD() {
+    start=$(date +%s)
+    $1
+    end=$(date +%s)
+    t=$(($end - $start))
+    printf 'Time taken: %d hours, %d minutes, %d seconds\n' "$(( t/60/60 ))"  "$(( t/60 - 60*(t/60/60) ))" "$(( t - 60*(t/60) ))"
+}
+
+# Run command with timing:
+timeCMD "sleep 4200"
+
+
